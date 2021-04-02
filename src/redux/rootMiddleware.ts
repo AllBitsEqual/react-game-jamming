41a99fd6bc3f4e0dc55afef2192de534e93abe6a
AllBitsEqual <mail@allbitsequal.com>
2021-04-02 22:06:09 +0200

    cleanup, api deleted

--- src/redux/rootMiddleware.ts
+++ src/redux/rootMiddleware.ts
@@ -1,11 +1,5 @@
-import { apiMiddleware } from './ducks/api'
-import { userInfoMiddleware } from './ducks/user-info'
-import { apiStoreMiddleware } from './ducks/api-store'
 import { routerMiddleware } from './router'
 
 export default [
     routerMiddleware,
-    apiMiddleware,
-    userInfoMiddleware,
-    apiStoreMiddleware,
 ]
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,13 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import apiReducer from './ducks/api'
-import apiStoreReducer from './ducks/api-store'
 import router from './router'
 import unitsReducer from './ducks/units'
 
 const rootReducer = combineReducers({
     router,
-    api: apiReducer,
-    apiStore: apiStoreReducer,
     units: unitsReducer,
 })
 

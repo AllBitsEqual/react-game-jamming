e0723cd80e78be1f16bd22d08dd8021efef7c767
AllBitsEqual <mail@allbitsequal.com>
2021-04-08 12:58:12 +0200

    added redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { default as styled, ThemeProvider } from 'styled-components'
 import { ConnectedRouter } from 'connected-react-router'
-import store from './redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './redux'
 import { history } from './redux/router'
 import Routes from './routes'
 import theme from './util/selectedTheme'
@@ -10,13 +11,15 @@ import theme from './util/selectedTheme'
 function App(): React.ReactElement {
     return (
         <Provider store={store}>
-            <ConnectedRouter history={history}>
-                <ThemeProvider theme={theme}>
-                    <BaseStyles>
-                        <Routes />
-                    </BaseStyles>
-                </ThemeProvider>
-            </ConnectedRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <ConnectedRouter history={history}>
+                    <ThemeProvider theme={theme}>
+                        <BaseStyles>
+                            <Routes />
+                        </BaseStyles>
+                    </ThemeProvider>
+                </ConnectedRouter>
+            </PersistGate>
         </Provider>
     )
 }
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,17 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import {
+    persistStore,
+    persistReducer,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './rootReducer'
 import middleware from './rootMiddleware'
 
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    whitelist: [],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 export const useReduxDispatch = (): AppDispatch => useDispatch<AppDispatch>()
 export const useReduxSelector: TypedUseSelectorHook<RootState> = useSelector
+
 export default store
